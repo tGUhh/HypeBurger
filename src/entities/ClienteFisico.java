8155b0a5dbc8534de53398bbb5e231bef4aeba1d
tGUhh <tiagocostasantos0013@gmail.com>
2022-01-24 21:16:38 -0300

    Correção de construtores

--- src/entities/ClienteFisico.java
+++ src/entities/ClienteFisico.java
@@ -1,32 +1,45 @@
 package entities;
 
+import java.util.Date;
+
 public class ClienteFisico extends Pessoa {
 	
 	protected Integer id;	
 	protected String nomeCompleto;
 	protected Long cpf;
-	protected int idade;
+	protected int idade;	
 	
-	public ClienteFisico() {
-	}
 
-	public ClienteFisico(Integer id, String nomeCompleto, Long cpf, int idade) {
-		this.id = id;
+	public ClienteFisico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+	}
+	
+	public ClienteFisico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String nomeCompleto, Long cpf, int idade) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.nomeCompleto = nomeCompleto;
 		this.cpf = cpf;
 		this.idade = idade;
 	}
 
-	public ClienteFisico(Integer id, String nomeCompleto, Long cpf) {
-		this.id = id;
+	public ClienteFisico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String nomeCompleto, Long cpf) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.nomeCompleto = nomeCompleto;
 		this.cpf = cpf;
-	}
+	}	
 
-	public ClienteFisico(Integer id, Long cpf) {
-		this.id = id;
+	public ClienteFisico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2, Long cpf) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.cpf = cpf;
-	}
+	}	
 
 	public Integer getId() {
 		return id;
@@ -58,9 +71,6 @@ public class ClienteFisico extends Pessoa {
 
 	public void setIdade(int idade) {
 		this.idade = idade;
-	}
-	
-	
+	}	
 
 }
-
--- src/entities/ClienteJuridico.java
+++ src/entities/ClienteJuridico.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Date;
+
 public class ClienteJuridico extends Pessoa {
 	
 	protected Integer id;	
@@ -7,44 +9,61 @@ public class ClienteJuridico extends Pessoa {
 	protected String nomeFantasia;
 	protected String cnpj;
 	protected String inscricaoEstadual;
-	protected String inscricaoMunicipal;
+	protected String inscricaoMunicipal;	
 	
-	public ClienteJuridico() {
-	}
 
-	public ClienteJuridico(Integer id, String razaoSocial, String nomeFantasia, String cnpj, String inscricaoEstadual,
-			String inscricaoMunicipal) {
-		this.id = id;
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+	}		
+
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String razaoSocial, String nomeFantasia, String cnpj, String inscricaoEstadual, String inscricaoMunicipal) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.razaoSocial = razaoSocial;
+		this.nomeFantasia = nomeFantasia;
+		this.cnpj = cnpj;
+		this.inscricaoEstadual = inscricaoEstadual;
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}		
+
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String nomeFantasia, String cnpj, String inscricaoEstadual, String inscricaoMunicipal) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.nomeFantasia = nomeFantasia;
 		this.cnpj = cnpj;
 		this.inscricaoEstadual = inscricaoEstadual;
 		this.inscricaoMunicipal = inscricaoMunicipal;
 	}
 
-	public ClienteJuridico(Integer id, String razaoSocial, String nomeFantasia, String cnpj, String inscricaoEstadual) {
-		this.id = id;
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String razaoSocial, String cnpj, String inscricaoEstadual) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.razaoSocial = razaoSocial;
-		this.nomeFantasia = nomeFantasia;
 		this.cnpj = cnpj;
 		this.inscricaoEstadual = inscricaoEstadual;
 	}
 
-	public ClienteJuridico(Integer id, String razaoSocial, String cnpj, String inscricaoMunicipal) {
-		this.id = id;
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String razaoSocial, String cnpj) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.razaoSocial = razaoSocial;
 		this.cnpj = cnpj;
-		this.inscricaoMunicipal = inscricaoMunicipal;
-	}
+	}	
 
-	public ClienteJuridico(Integer id, String razaoSocial, String cnpj) {
-		this.id = id;
-		this.razaoSocial = razaoSocial;
-		this.cnpj = cnpj;
-	}
-
-	public ClienteJuridico(Integer id, String cnpj) {
-		this.id = id;
+	public ClienteJuridico(Integer id, Date dataCadastro, String endereco, String estado, String cidade, String bairro,
+			String cep, String telefone, String celular, String email, String pontoReferencia, Integer id2,
+			String cnpj) {
+		super(id, dataCadastro, endereco, estado, cidade, bairro, cep, telefone, celular, email, pontoReferencia);
+		id = id2;
 		this.cnpj = cnpj;
 	}
 
@@ -95,8 +114,6 @@ public class ClienteJuridico extends Pessoa {
 	public void setInscricaoMunicipal(String inscricaoMunicipal) {
 		this.inscricaoMunicipal = inscricaoMunicipal;
 	}
-	
-	
 	
 	
 }
